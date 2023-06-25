fdf02c641956b918adc517825c6cf4b852770a0f
Gabriel Pavão <gabpavao@outlook.com>
2023-06-24 22:19:30 -0300

    feat: Apache Kafka set up and process

---
+++ order-book-golang/cmd/trade/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/gabrielgpavao/home-broker-fullstack/order-book/internal/infra/kafka"
+	"github.com/gabrielgpavao/home-broker-fullstack/order-book/internal/market/dto"
+	"github.com/gabrielgpavao/home-broker-fullstack/order-book/internal/market/entity"
+	"github.com/gabrielgpavao/home-broker-fullstack/order-book/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id": "myGroup",
+		"auto.offset.reset": "latest",
+	}
+
+	producer := kafka.NewKafkaProducer(configMap)
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consume(kafkaMsgChan)
+
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade()
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1)
+
+			fmt.Println(string(msg.Value))
+
+			tradeInput := dto.TradeInput{}
+
+			err := json.Unmarshal(msg.Value, &tradeInput)
+
+			if err != nil {
+				panic(err)
+
+			}
+			order := transformer.TransformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+
+		fmt.Println(string(outputJson))
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
